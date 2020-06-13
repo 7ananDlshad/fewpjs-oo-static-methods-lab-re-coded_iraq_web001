98fc79cbacf6cb0c2326ffb4e17b28a842cf2151
Hanan Dlshad <ensw03217@student.su.edu.krd>
2020-06-13 11:08:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  
+  static titleize(statement){
+    let words = string.split(' ');
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let n = 0; n < words.length; n++) {
+      if(words[n] !== except){
+      words[n] = words[n].charAt(0).toUpperCase() + words[n].slice(1);
+    }
+      else{
+        words[n] = words[n].slice(0)
+      }
+    }
+    return words.join(' ');
+  }
 }
